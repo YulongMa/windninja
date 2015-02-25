3f5d49cecfcbd0f734c399557b80ef32ff581b71
Kyle Shannon <kyle@pobox.com>
2015-02-25 16:03:10 -0700

    commit some code to the cpp classes

--- src/gui/ninjamainwindow.h
+++ src/gui/ninjamainwindow.h
@@ -1,3 +1,30 @@
+/******************************************************************************
+ *
+ * Project:  WindNinja
+ * Purpose:  Main window for Qt
+ * Author:   Kyle Shannon <kyle at pobox dot com>
+ *
+ ******************************************************************************
+ *
+ * THIS SOFTWARE WAS DEVELOPED AT THE ROCKY MOUNTAIN RESEARCH STATION (RMRS)
+ * MISSOULA FIRE SCIENCES LABORATORY BY EMPLOYEES OF THE FEDERAL GOVERNMENT 
+ * IN THE COURSE OF THEIR OFFICIAL DUTIES. PURSUANT TO TITLE 17 SECTION 105 
+ * OF THE UNITED STATES CODE, THIS SOFTWARE IS NOT SUBJECT TO COPYRIGHT 
+ * PROTECTION AND IS IN THE PUBLIC DOMAIN. RMRS MISSOULA FIRE SCIENCES 
+ * LABORATORY ASSUMES NO RESPONSIBILITY WHATSOEVER FOR ITS USE BY OTHER 
+ * PARTIES,  AND MAKES NO GUARANTEES, EXPRESSED OR IMPLIED, ABOUT ITS QUALITY, 
+ * RELIABILITY, OR ANY OTHER CHARACTERISTIC.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
+
 #ifndef NINJAMAINWINDOW_H
 #define NINJAMAINWINDOW_H
 
@@ -10,13 +37,14 @@ class NinjaMainWindow;
 class NinjaMainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
-    explicit NinjaMainWindow(QWidget *parent = 0);
+    explicit NinjaMainWindow( QWidget *parent = 0 );
     ~NinjaMainWindow();
-    
+
 private:
     Ui::NinjaMainWindow *ui;
 };
 
-#endif // NINJAMAINWINDOW_H
+#endif /* NINJAMAINWINDOW_H */
+
